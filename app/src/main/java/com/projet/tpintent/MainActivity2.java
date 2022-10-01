76bedea18772a5b44764eda1f7f0143ac8d6c0ee
AnwarBensouissi <anwar777bensouissi@gmail.com>
2022-10-02 00:07:40 +0100

    add return button

--- app/src/main/java/com/projet/tpintent/MainActivity2.java
+++ app/src/main/java/com/projet/tpintent/MainActivity2.java
@@ -2,16 +2,27 @@ package com.projet.tpintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
     private TextView tv;
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         tv=findViewById(R.id.tv);
         tv.setText("Hi "+getIntent().getStringExtra("Name"));
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity2.this, MainActivity.class);
+               startActivity(it);
+            }
+        });
     }
 }
